205730e8518591b7f7fc5bb23b8c062a5231a939
sugarkwork <sugarkwork@sugar-knight.com>
2025-01-10 00:08:01 +0900

    test

--- nodes.py
+++ nodes.py
@@ -1,84 +1,79 @@
 from PIL import Image
-from pytoshop import layers
-from pytoshop.enums import BlendMode
-from pytoshop.core import PsdFile
-import numpy as np
+from psd_tools import PSDImage
+from psd_tools.api.layers import PixelLayer, BlendMode
 
 
-class PsdLayer:
-    def __init__(self, image: Image, name: str, blending_mode: BlendMode = BlendMode.normal):
-        self.image = image.convert("RGBA")
-        self.image_data = np.array(self.image)
-        self.name = name
-        self.blending_mode = blending_mode
+class PSDData:
+    class _Layer:
+        def __init__(self, image:Image, name:str, blend_mode:BlendMode):
+            self.image = image
+            self.name = name
+            self.blend_mode = blend_mode
 
-
-class Psd:
     def __init__(self):
-        self.psd_file = None
+        self.layers = []
+    
+    def append(self, image:Image, name:str, blend_mode:BlendMode):
+        self.layers.append(self._Layer(image, name, blend_mode))
 
-    def add_image(self, image: Image, name: str, blending_mode: BlendMode = BlendMode.normal) -> PsdLayer:
-        layer = PsdLayer(image, name, blending_mode)
-        self.add_layer(layer)
-        return layer
+    def save(self, path):
+        max_width = 0
+        max_height = 0
+        for layer in self.layers:
+            layer_width = layer.image.size.width
+            layer_height = layer.image.size.height
+            if layer_width > max_width:
+                max_width = layer_width
+            if layer_height > max_height:
+                max_height = layer_height
 
-    def add_layer(self, layer: PsdLayer):
-        if self.psd_file is None:
-            self.psd_file = PsdFile(num_channels=3, height=layer.image_data.shape[0], width=layer.image_data.shape[1])
+        psd = PSDImage.new(size=(max_width, max_height), mode='RGBA')
 
-        channel_data = [layers.ChannelImageData(image=layer.image_data[:, :, i], compression=1) for i in range(4)]
+        for layer in self.layers:
+            layer = PixelLayer.frompil(layer.image, psd, layer.name)
+            layer.blend_mode = layer.blend_mode
+            psd.append(layer)
 
-        layer_record = layers.LayerRecord(
-            channels={-1: channel_data[3], 0: channel_data[0], 1: channel_data[1], 2: channel_data[2]},
-            top=0, bottom=layer.image_data.shape[0], left=0, right=layer.image_data.shape[1],
-            blend_mode=layer.blending_mode,
-            name=layer.name,
-            opacity=255,
-        )
-
-        self.psd_file.layer_and_mask_info.layer_info.layer_records.append(layer_record)
-
-    def save(self, output_path: str):
-        with open(output_path, 'wb') as output_file:
-            self.psd_file.write(output_file)
+        psd.save(path)
 
 
-class SavePSD:
+class PSDLayer:
     def __init__(self):
         pass
-    
+
     @classmethod
     def INPUT_TYPES(s):
+        blend_modes = [x.name for x in BlendMode]
         return {
             "required": {
+                "image": ("IMAGE",),
                 "name": ("STRING",),
-                "image1": ("IMAGE",),
-              "optional": {
-                "image2": ("IMAGE",),
-                "image3": ("IMAGE",),
-                "image4": ("IMAGE",),
-                "image5": ("IMAGE",),
-                "image6": ("IMAGE",),
-                "image7": ("IMAGE",),
-                "image8": ("IMAGE",),
-                "image9": ("IMAGE",),
-                },
+                "psd": ("PSD", {"default": None}),
+                "blend_mode": (blend_modes, { "default": BlendMode.NORMAL.name }),
             },
         }
+    
+    RETURN_TYPES = ("PSD",)
 
-    RETURN_TYPES = ()
-    FUNCTION = "save_psd"
+    FUNCTION = "tag"
+    OUTPUT_NODE = True
 
-    CATEGORY = "Example"
+    CATEGORY = "image"
 
-    def save_psd(self, name, image1, image2=None, image3=None, image4=None, image5=None, image6=None, image7=None, image8=None, image9=None):
-        print(f"save_psd: {name}")
-        return
+    def tag(self, image, name, psd=None,  blend_mode=BlendMode.NORMAL.name):
+        if psd is None:
+            psd = PSDData()
+
+        psd.append(image, name, BlendMode[blend_mode])
+        psd.save("output.psd")
+
+        return (psd,)
+
 
 NODE_CLASS_MAPPINGS = {
-    "SavePSD": SavePSD
+    "PSDLayer": PSDLayer
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SavePSD": "SavePSD Node"
+    "PSDLayer": "PSDLayer"
 }
