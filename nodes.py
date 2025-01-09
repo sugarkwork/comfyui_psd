1d4b5e3d9876f0ad471afef8a3d8c8256f91a7e4
sugarkwork <sugarkwork@sugar-knight.com>
2025-01-10 00:22:59 +0900

    test2

--- nodes.py
+++ nodes.py
@@ -20,8 +20,8 @@ class PSDData:
         max_width = 0
         max_height = 0
         for layer in self.layers:
-            layer_width = layer.image.size.width
-            layer_height = layer.image.size.height
+            layer_width = layer.image.size[0]
+            layer_height = layer.image.size[1]
             if layer_width > max_width:
                 max_width = layer_width
             if layer_height > max_height:
@@ -30,9 +30,9 @@ class PSDData:
         psd = PSDImage.new(size=(max_width, max_height), mode='RGBA')
 
         for layer in self.layers:
-            layer = PixelLayer.frompil(layer.image, psd, layer.name)
-            layer.blend_mode = layer.blend_mode
-            psd.append(layer)
+            add_layer = PixelLayer.frompil(layer.image, psd, layer.name)
+            add_layer.blend_mode = layer.blend_mode
+            psd.append(add_layer)
 
         psd.save(path)
 
@@ -63,8 +63,14 @@ class PSDLayer:
     def tag(self, image, name, psd=None,  blend_mode=BlendMode.NORMAL.name):
         if psd is None:
             psd = PSDData()
+        
+        blend_mode_data = BlendMode.NORMAL
+        for mode in BlendMode:
+            if mode.name == blend_mode:
+                blend_mode_data = mode
+                break
 
-        psd.append(image, name, BlendMode[blend_mode])
+        psd.append(image, name, blend_mode_data)
         psd.save("output.psd")
 
         return (psd,)
@@ -77,3 +83,23 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PSDLayer": "PSDLayer"
 }
+
+
+def simple_test():
+    from PIL import Image
+    image = Image.new("RGB", (100, 100), (255, 0, 0))
+
+    psdlayer1 = PSDLayer()
+    psd = psdlayer1.tag(image, "test1", blend_mode="NORMAL")
+
+    psdlayer2 = PSDLayer()
+    psd = psdlayer2.tag(Image.open("test01.png"), "test2", psd=psd[0], blend_mode="NORMAL")
+
+    psdlayer3 = PSDLayer()
+    psd = psdlayer3.tag(Image.open("test02.png"), "test3", psd=psd[0], blend_mode="SCREEN")
+
+    psd[0].save("output.psd")
+
+
+#if __name__ == "__main__":
+#    simple_test()
