bfa53df6fe28d2f9dbc20d3ff42ccf81e39a6f30
sugarkwork <sugarkwork@sugar-knight.com>
2025-01-10 14:08:01 +0900

    install Cython and update psd-tools in installation script

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 ..\..\..\python_embeded\python.exe -m pip install -U https://www.piwheels.org/simple/docopt/docopt-0.6.2-py2.py3-none-any.whl#sha256=15fde8252aa9f2804171014d50d069ffbf42c7a50b7d74bcbb82bfd5700fcfc2
 
 ..\..\..\python_embeded\python.exe -m pip uninstall psd-tools
+..\..\..\python_embeded\python.exe -m pip install Cython
 ..\..\..\python_embeded\python.exe -m pip install -U psd-tools
 ..\..\..\python_embeded\python.exe -m pip install -U git+https://github.com/psd-tools/psd-tools.git
 
